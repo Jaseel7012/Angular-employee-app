4327321dac61851d1efe189d1c4e2ed001ab3026
jaseel <jaseeljctcs@gmail.com>
2022-11-29 02:09:41 +0530

    add emp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
+
+const myroute:Routes=[
+  {
+    path:'',
+    component:EmployeeAddComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
